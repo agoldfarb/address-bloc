13d6d91d9a8aa577a6cd021790ae2208303af8f7
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-06-07 23:01:02 -0500

    Checkpoint 22 completed, Address Bloc Menu

--- models/address_book.rb
+++ models/address_book.rb
@@ -2,12 +2,12 @@ require_relative "entry.rb"
 
 class AddressBook
   attr_accessor :entries
-
   def initialize
     @entries = []
   end
 
   def add_entry(name, phone, email)
+    
     index = 0
     @entries.each do |entry|
       if name < entry.name
@@ -15,6 +15,7 @@ class AddressBook
       end
       index += 1
     end
+
     @entries.insert(index, Entry.new(name, phone, email))
   end
 end
